bfb907681894a9095414ea03c88f22eb3351c5f6
Angie <angiejimenez@gmail.com>
2022-07-02 18:10:38 +0200

    impor HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { FooterComponent } from './footer/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ClientsComponent } from './clients/clients.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ClientsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
