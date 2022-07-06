0d3120b02e4490a1fbd88d1e4ea77d32ff396d4b
Angie <angiejimenez@gmail.com>
2022-07-06 16:38:20 +0200

    create method correction

--- src/app/clients/form.component.ts
+++ src/app/clients/form.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Client } from '../models/client';
 import { ClientService } from '../service/client.service';
 import swal from 'sweetalert2';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -12,8 +13,13 @@ import swal from 'sweetalert2';
 
 })
 export class FormComponent implements OnInit {
+
+  @ViewChild('form')
+  form!: NgForm;
+
   public client = new Client(0, '', '', '', '');
   public title: string = 'Create Clients';
+
 
   constructor(
     private clientSrv: ClientService,
@@ -24,6 +30,13 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
     this.loadClient();
   }
+
+
+    existClient(): boolean{
+    return this.client.id != 0;
+
+  }
+
 
   loadClient(): void {
     this.activatedRoute.params.subscribe((params) => {
