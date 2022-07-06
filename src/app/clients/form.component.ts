8f654d4995d6543d6739a0c2e804dbc9532dd334
Angie <angiejimenez@gmail.com>
2022-07-06 08:55:16 +0200

    install sweetalert2

--- src/app/clients/form.component.ts
+++ src/app/clients/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Client } from '../models/client';
 import { ClientService } from '../service/client.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -12,15 +13,19 @@ export class FormComponent implements OnInit {
   public client = new Client(0, '', '', '', '');
   public title: string = 'Create Clients';
 
-  constructor(private clientSrv : ClientService,
-              private router: Router) {}
 
-  ngOnInit(): void {}
+
+  constructor(private clientSrv : ClientService,
+              private router: Router) {}  ngOnInit(): void {}
 
       create(): void {
         this.clientSrv.create(this.client)
-        .subscribe(response => this.router.navigate(['/clients']));
-        console.log( this.client );
+        .subscribe(response => {
+          this.router.navigate(['/clients'])
+          swal.fire('Client Created', `Client ${this.client.name} created successfully`, 'success');
+
+        }
+        );console.log( this.client );
       }
 }
 
